e3c30a6d30d6baae1d5481e5fb6afe191a278f4a
ZShUn <616394599@qq.com>
2020-02-10 23:42:38 +0800

    1.添加username password参数

--- nacos-config-spring-boot-autoconfigure/src/main/java/com/alibaba/boot/nacos/config/properties/NacosConfigProperties.java
+++ nacos-config-spring-boot-autoconfigure/src/main/java/com/alibaba/boot/nacos/config/properties/NacosConfigProperties.java
@@ -70,11 +70,32 @@ public class NacosConfigProperties {
 
 	private boolean enableRemoteSyncConfig = false;
 
+	private String username;
+
+	private String password;
+
 	@JSONField(serialize = false)
 	private List<Config> extConfig = new ArrayList<>();
 
 	@NestedConfigurationProperty
 	private Bootstrap bootstrap = new Bootstrap();
+
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public String getServerAddr() {
 		return serverAddr;
