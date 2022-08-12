ad81bbdfc2de4dcf4d9480e03cb0cfdceb69cafe
chaos <2499904609@qq.com>
2022-08-12 17:19:53 +0800

    add some junit test

---
+++ nacos-config-spring-boot-autoconfigure/src/test/java/com/alibaba/boot/nacos/util/AttributeExtractTaskTest.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.boot.nacos.util;
+
+import com.alibaba.boot.nacos.config.autoconfigure.NacosConfigAutoConfiguration;
+import com.alibaba.boot.nacos.config.util.AttributeExtractTask;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+/**
+ * {@link AttributeExtractTask} Test
+ * @ClassName: AttributeExtractTaskTest
+ * @Author: ChenHao26
+ * @Date: 2022/8/12 15:08
+ * @Description: TODO
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestPropertySource(properties = { "nacos.config.server-addr=localhost" })
+@SpringBootTest(classes = { NacosConfigAutoConfiguration.class })
+public class AttributeExtractTaskTest {
+    
+    private AttributeExtractTask attributeExtractTask;
+    
+    @Autowired
+    private ConfigurableEnvironment environment ;
+    
+    @Before
+    public void setUp() {
+        environment.setDefaultProfiles("prefix01");
+        attributeExtractTask = new AttributeExtractTask("prefix",environment);
+    }
+    
+    @Test
+    public void call(){
+        try {
+            attributeExtractTask.call();
+        }catch (Exception e) {
+            Assert.assertNotNull(e);
+        }
+    }
+}
